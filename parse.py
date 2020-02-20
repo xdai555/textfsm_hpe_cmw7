8fe4d18056fa607bf66a1b0dfa4e96b49ace83ed
odai5 <vip@odai.vip>
2020-02-20 23:46:43 +0800

    first commit

---
+++ parse.py
@@ -0,0 +1,10 @@
+from textfsm import clitable
+from netmiko import ConnectHandler
+from pprint import pprint
+
+def parse(cmd):
+    cli_table = clitable.CliTable('index','templates')
+    attr = {'Command':cmd,'Vendor':'hp_comware'}
+    with open('raw_data') as f:
+        cli_table.ParseCmd(f.read(),attr)
+    print(cli_table)
